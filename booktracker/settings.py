f168ceaf7b680ddae16e3096cbdc908a4104a755
jrxmae <leah.mullin9@gmail.com>
2025-01-14 06:22:50 +0000

    Add secret key

--- booktracker/settings.py
+++ booktracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$okt39ma@vele^keaqgk(*#@$-%-(*6pr$y5r*n0j+q(&z8umc'
+SECRET_KEY = os.environ.get("SECRET_KEY", "f941r6y@*h3cym@i31gzuq@#-&a3hd@lal=n1$&=%!#5l&=hdw")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
