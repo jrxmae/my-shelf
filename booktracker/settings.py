df28845575bfefbfcfae78221b9c7527c358832e
jrxmae <leah.mullin9@gmail.com>
2025-01-15 23:19:56 +0000

    Add Heroku domain to ALLOWED_HOSTS

--- booktracker/settings.py
+++ booktracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "f941r6y@*h3cym@i31gzuq@#-&a3hd@lal=n1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jrxmae-myshelf-m15pn6o4xxu.ws-eu117.gitpod.io','.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-jrxmae-myshelf-m15pn6o4xxu.ws-eu117.gitpod.io','my-shelf-f8680836df52.herokuapp.com',]
 
 
 # Application definition
